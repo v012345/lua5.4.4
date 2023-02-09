d3511dd4473849eebe8ffbba38f7f322cfcaefbf
v012345 <v138468@gmail.com>
2023-02-09 16:13:25 +0800

    修正项目

--- utils/LoadConfigFromLuaScripts/LoadConfigFromLuaScripts.cpp
+++ utils/LoadConfigFromLuaScripts/LoadConfigFromLuaScripts.cpp
@@ -1,5 +1,9 @@
+#include <lua.hpp>
+#include <iostream>
 int main(int argc, char const *argv[])
 {
-    /* code */
+    std::cout << argv[1] << std::endl;
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
     return 0;
 }
