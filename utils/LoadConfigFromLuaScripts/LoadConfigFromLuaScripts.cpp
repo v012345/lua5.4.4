3e88cd0c619397523c892eac2b2673b1575002f7
v012345 <v138468@gmail.com>
2023-02-10 16:43:16 +0800

    加入新 api

--- utils/LoadConfigFromLuaScripts/LoadConfigFromLuaScripts.cpp
+++ utils/LoadConfigFromLuaScripts/LoadConfigFromLuaScripts.cpp
@@ -8,6 +8,7 @@ static int GetFilesInfoInDirectory(lua_State *L);
 static int CopyFile(lua_State *L);
 static int GetMainLuaFilePath(lua_State *L);
 static int GetFileMd5(lua_State *L);
+static int IsFileExist(lua_State *L);
 
 std::string sMainLuaFilePath;
 
@@ -18,7 +19,6 @@ int main(int argc, char const *argv[])
 
         std::cout << "enter " << argv[1] << std::endl;
         sMainLuaFilePath = argv[1];
-        std::cout << getFileMD5("C:\\Users\\Meteor\\Desktop\\aa.txt") << std::endl;
         lua_State *L = luaL_newstate();
         luaL_openlibs(L);
         lua_register(L, "GetFileLastModifiedTimestamp", GetFileLastModifiedTimestamp);
@@ -26,6 +26,7 @@ int main(int argc, char const *argv[])
         lua_register(L, "CopyFile", CopyFile);
         lua_register(L, "GetMainLuaFilePath", GetMainLuaFilePath);
         lua_register(L, "GetFileMd5", GetFileMd5);
+        lua_register(L, "IsFileExist", IsFileExist);
         luaL_dofile(L, argv[1]);
         std::cout << "leave " << argv[1] << std::endl;
     }
@@ -46,7 +47,6 @@ static int GetFilesInfoInDirectory(lua_State *L)
     size_t i = 1;
     for (auto &&directoryOrFile : std::filesystem::directory_iterator(std::filesystem::path(lua_tostring(L, 1))))
     {
-        std::cout << i << std::endl;
         lua_pushinteger(L, i);
         lua_newtable(L);
         {
@@ -63,6 +63,15 @@ static int GetFilesInfoInDirectory(lua_State *L)
             {
                 lua_pushstring(L, "filename");
                 lua_pushstring(L, directoryOrFile.path().filename().string().c_str());
+                lua_settable(L, -3);
+            }
+            {
+
+                lua_pushstring(L, "md5");
+                if (directoryOrFile.is_directory())
+                    lua_pushstring(L, "");
+                else
+                    lua_pushstring(L, getFileMD5(directoryOrFile.path().string()).c_str());
                 lua_settable(L, -3);
             }
         }
@@ -96,5 +105,12 @@ static int GetFileMd5(lua_State *L)
 {
     const char *file = lua_tostring(L, 1);
     lua_pushstring(L, getFileMD5(file).c_str());
+    return 1;
+}
+
+static int IsFileExist(lua_State *L)
+{
+    const char *file = lua_tostring(L, 1);
+    lua_pushboolean(L, std::filesystem::exists(file));
     return 1;
 }
