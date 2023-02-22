aa7b7f2cd5200c29753c484ac30388ddb8f932d4
v012345 <v138468@gmail.com>
2023-02-22 11:42:23 +0800

    重构

--- my_tools/myLua/include/nightowl_cpp_api.hpp
+++ my_tools/myLua/include/nightowl_cpp_api.hpp
@@ -23,15 +23,17 @@ namespace NIGHTOWL
         XML(std::string file_path);
         const std::string &GET_PATH() const { return file_path; }
         static int REGISTER_TO_LUA(lua_State *L);
-        static int CREATE_OBJECT(lua_State *L);
-        static int DESTROY_OBJECT(lua_State *L);
+        static int CREATE(lua_State *L);
+        static int DESTROY(lua_State *L);
         static int GET_PATH(lua_State *L);
-        static XML *GET_XML(lua_State *L, int arg);
+        static XML *GET(lua_State *L, int arg);
         static const luaL_Reg METHODS_MAP[];
         ~XML();
     };
 
     void REGISTER_LIBS_TO_LUA(lua_State *L);
-    static const luaL_Reg LIBS[];
+    static const luaL_Reg LIBS[] = {
+        {"XML", XML::REGISTER_TO_LUA},
+        {NULL, NULL}};
 
 }
--- my_tools/myLua/src/nightowl_cpp_api.cpp
+++ my_tools/myLua/src/nightowl_cpp_api.cpp
@@ -7,28 +7,23 @@ namespace NIGHTOWL
     {
         this->file_path = file_path;
     }
-
     int XML::GET_PATH(lua_State *L)
     {
-        XML *pStu = XML::GET_XML(L, 1);
+        XML *xml = XML::GET(L, 1);
 
-        const std::string &name = pStu->GET_PATH();
-        lua_pushstring(L, name.c_str());
+        const std::string &path = xml->GET_PATH();
+        lua_pushstring(L, path.c_str());
         return 1;
     }
-
     XML::~XML()
     {
     }
 
-    static const luaL_Reg LIBS[] = {
-        {"XML", XML::REGISTER_TO_LUA},
-        {NULL, NULL}};
 
     const luaL_Reg XML::METHODS_MAP[] = {
         {"getPath", XML::GET_PATH},
-        {"new", XML::CREATE_OBJECT},
-        {"__gc", XML::DESTROY_OBJECT},
+        {"new", XML::CREATE},
+        {"__gc", XML::DESTROY},
         {NULL, NULL}};
 
     void REGISTER_LIBS_TO_LUA(lua_State *L)
@@ -41,14 +36,12 @@ namespace NIGHTOWL
         }
     }
 
-    int XML::CREATE_OBJECT(lua_State *L)
+    int XML::CREATE(lua_State *L)
     {
 
         std::string path = luaL_checkstring(L, -1);
-        // 创建userdata，搞到对象指针
         XML **ppStu = (XML **)lua_newuserdata(L, sizeof(XML));
         (*ppStu) = new XML(path);
-        // 获取元表
         luaL_getmetatable(L, "XML");
         lua_setmetatable(L, -2);
         return 1;
@@ -63,19 +56,18 @@ namespace NIGHTOWL
         return 1;
     }
 
-    XML *XML::GET_XML(lua_State *L, int arg)
+    XML *XML::GET(lua_State *L, int arg)
     {
-        // 从栈顶取userdata，这个是C++的对象指针
         luaL_checktype(L, arg, LUA_TUSERDATA);
         void *userData = luaL_checkudata(L, arg, "XML");
         luaL_argcheck(L, userData != NULL, 1, "user data error");
         return *(XML **)userData;
     }
-    int XML::DESTROY_OBJECT(lua_State *L)
+    int XML::DESTROY(lua_State *L)
     {
-        XML *pStu = XML::GET_XML(L, 1);
-        if (pStu)
-            delete pStu;
+        XML *xml = XML::GET(L, 1);
+        if (xml)
+            delete xml;
         return 1;
     }
 
