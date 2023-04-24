73aced86bbfc03fb2b57d63e978d06c53050d8b0
v012345 <v138468@gmail.com>
2023-04-24 18:34:01 +0800

    加入 bytedump

---
+++ scripts/lua/bytedump.lua
@@ -0,0 +1,264 @@
+Bytedump = {
+    A_pos = 7,
+    A_mask = 0x7F80,
+    B_pos = 16,
+    B_mask = 0xFF0000,
+    Bx_pos = 15,
+    Bx_mask = 0xFFFF8000,
+    sBx_pos = 15,
+    sBx_offset = 0xFFFF,
+    C_pos = 24,
+    C_mask = 0xFF000000,
+}
+function Bytedump:sBx(code)
+    return ((code & self.Bx_mask) >> self.sBx_pos) - self.sBx_offset
+end
+
+function Bytedump:A(code)
+    return (code & self.A_mask) >> self.A_pos
+end
+
+function Bytedump:B(code)
+    return (code & self.B_mask) >> self.B_pos
+end
+
+function Bytedump:Bx(code)
+    return (code & self.Bx_mask) >> self.Bx_pos
+end
+
+function Bytedump:C(code)
+    return (code & self.C_mask) >> self.C_pos
+end
+
+local OP_CODE = {
+    "OP_MOVE",
+    "OP_LOADI",
+    "OP_LOADF",
+    "OP_LOADK",
+    "OP_LOADKX",
+    "OP_LOADFALSE",
+    "OP_LFALSESKIP",
+    "OP_LOADTRUE",
+    "OP_LOADNIL",
+    "OP_GETUPVAL",
+    "OP_SETUPVAL",
+    "OP_GETTABUP",
+    "OP_GETTABLE",
+    "OP_GETI",
+    "OP_GETFIELD",
+    "OP_SETTABUP",
+    "OP_SETTABLE",
+    "OP_SETI",
+    "OP_SETFIELD",
+    "OP_NEWTABLE",
+    "OP_SELF",
+    "OP_ADDI",
+    "OP_ADDK",
+    "OP_SUBK",
+    "OP_MULK",
+    "OP_MODK",
+    "OP_POWK",
+    "OP_DIVK",
+    "OP_IDIVK",
+    "OP_BANDK",
+    "OP_BORK",
+    "OP_BXORK",
+    "OP_SHRI",
+    "OP_SHLI",
+    "OP_ADD",
+    "OP_SUB",
+    "OP_MUL",
+    "OP_MOD",
+    "OP_POW",
+    "OP_DIV",
+    "OP_IDIV",
+    "OP_BAND",
+    "OP_BOR",
+    "OP_BXOR",
+    "OP_SHL",
+    "OP_SHR",
+    "OP_MMBIN",
+    "OP_MMBINI",
+    "OP_MMBINK",
+    "OP_UNM",
+    "OP_BNOT",
+    "OP_NOT",
+    "OP_LEN",
+    "OP_CONCAT",
+    "OP_CLOSE",
+    "OP_TBC",
+    "OP_JMP",
+    "OP_EQ",
+    "OP_LT",
+    "OP_LE",
+    "OP_EQK",
+    "OP_EQI",
+    "OP_LTI",
+    "OP_LEI",
+    "OP_GTI",
+    "OP_GEI",
+    "OP_TEST",
+    "OP_TESTSET",
+    "OP_CALL",
+    "OP_TAILCALL",
+    "OP_RETURN",
+    "OP_RETURN0",
+    "OP_RETURN1",
+    "OP_FORLOOP",
+    "OP_FORPREP",
+    "OP_TFORPREP",
+    "OP_TFORCALL",
+    "OP_TFORLOOP",
+    "OP_SETLIST",
+    "OP_CLOSURE",
+    "OP_VARARG",
+    "OP_VARARGPREP",
+    "OP_EXTRAARG"
+}
+
+local OP_ACT = {
+    OP_MOVE = function(code)
+        local f = "%s R[%s] => R[%s]"
+        local name = OP_CODE[(code & 0x7F) + 1]
+        local B = Bytedump:B(code)
+        local A = Bytedump:A(code)
+        print(string.format(f, name, B, A))
+    end,
+    OP_LOADI = function(code)
+        local f = "%s sBx:%s => R[%s]"
+        local name = OP_CODE[(code & 0x7F) + 1]
+        local sBx = Bytedump:sBx(code)
+        local A = Bytedump:A(code)
+        print(string.format(f, name, sBx, A))
+    end,
+    OP_LOADF = nil,
+    OP_LOADK = function(code)
+        local f = "%s K[%s] => R[%s]"
+        local name = OP_CODE[(code & 0x7F) + 1]
+        local A = Bytedump:A(code)
+        local B = Bytedump:B(code)
+        print(string.format(f, name, B, A))
+    end,
+    OP_LOADKX = nil,
+    OP_LOADFALSE = function(code)
+        local f = "%s false => R[%s]"
+        local name = OP_CODE[(code & 0x7F) + 1]
+        local A = Bytedump:A(code)
+        print(string.format(f, name, A))
+    end,
+    OP_LFALSESKIP = nil,
+    OP_LOADTRUE = function(code)
+        local f = "%s true => R[%s]"
+        local name = OP_CODE[(code & 0x7F) + 1]
+        local A = Bytedump:A(code)
+        print(string.format(f, name, A))
+    end,
+    OP_LOADNIL = nil,
+    OP_GETUPVAL = nil,
+    OP_SETUPVAL = nil,
+    OP_GETTABUP = function(code)
+        local f = "%s UpValue[%s][K[%s]] => R[%s]"
+        local name = OP_CODE[(code & 0x7F) + 1]
+        local A = Bytedump:A(code)
+        local B = Bytedump:B(code)
+        local C = Bytedump:C(code)
+        print(string.format(f, name, B, C, A))
+    end,
+    OP_GETTABLE = nil,
+    OP_GETI = nil,
+    OP_GETFIELD = nil,
+    OP_SETTABUP = nil,
+    OP_SETTABLE = nil,
+    OP_SETI = nil,
+    OP_SETFIELD = nil,
+    OP_NEWTABLE = nil,
+    OP_SELF = nil,
+    OP_ADDI = nil,
+    OP_ADDK = nil,
+    OP_SUBK = nil,
+    OP_MULK = nil,
+    OP_MODK = nil,
+    OP_POWK = nil,
+    OP_DIVK = nil,
+    OP_IDIVK = nil,
+    OP_BANDK = nil,
+    OP_BORK = nil,
+    OP_BXORK = nil,
+    OP_SHRI = nil,
+    OP_SHLI = nil,
+    OP_ADD = nil,
+    OP_SUB = nil,
+    OP_MUL = nil,
+    OP_MOD = nil,
+    OP_POW = nil,
+    OP_DIV = nil,
+    OP_IDIV = nil,
+    OP_BAND = nil,
+    OP_BOR = nil,
+    OP_BXOR = nil,
+    OP_SHL = nil,
+    OP_SHR = nil,
+    OP_MMBIN = nil,
+    OP_MMBINI = nil,
+    OP_MMBINK = nil,
+    OP_UNM = nil,
+    OP_BNOT = nil,
+    OP_NOT = nil,
+    OP_LEN = nil,
+    OP_CONCAT = nil,
+    OP_CLOSE = nil,
+    OP_TBC = nil,
+    OP_JMP = nil,
+    OP_EQ = nil,
+    OP_LT = nil,
+    OP_LE = nil,
+    OP_EQK = nil,
+    OP_EQI = nil,
+    OP_LTI = nil,
+    OP_LEI = nil,
+    OP_GTI = nil,
+    OP_GEI = nil,
+    OP_TEST = nil,
+    OP_TESTSET = nil,
+    OP_CALL = function(code)
+        local f = "%s R[%s](arg * B:%s) {return * (C:%s - 1)}"
+        local name = OP_CODE[(code & 0x7F) + 1]
+        local A = Bytedump:A(code)
+        local B = Bytedump:B(code)
+        local C = Bytedump:C(code)
+        print(string.format(f, name, A, B, C))
+    end,
+    OP_TAILCALL = nil,
+    OP_RETURN = nil,
+    OP_RETURN0 = nil,
+    OP_RETURN1 = nil,
+    OP_FORLOOP = nil,
+    OP_FORPREP = nil,
+    OP_TFORPREP = nil,
+    OP_TFORCALL = nil,
+    OP_TFORLOOP = nil,
+    OP_SETLIST = nil,
+    OP_CLOSURE = nil,
+    OP_VARARG = nil,
+    OP_VARARGPREP = function(code)
+        local f = "%s A:%s"
+        local name = OP_CODE[(code & 0x7F) + 1]
+        local A = Bytedump:A(code)
+        print(string.format(f, name, A))
+    end,
+    OP_EXTRAARG = nil,
+}
+
+for _, file in pairs(GetOpCodes()) do
+    print(file.script_name)
+    for _, code in pairs(file) do
+        if type(code) == "number" then
+            local act = OP_CODE[(code & 0x7F) + 1]
+            if OP_ACT[act] then
+                OP_ACT[act](code)
+            else
+                print(act)
+            end
+        end
+    end
+end
--- scripts/lua/parser_example.lua
+++ scripts/lua/parser_example.lua
@@ -1,11 +1,6 @@
-local counter, step = 10, 1
-counter = 12
-step = 6
 
-for index, value in pairs(GetOpCodes()) do
-    for key, value1 in pairs(value) do
-        if type(value1) == "number" then
-            print(value1 & 0x7F)
-        end
-    end
-end
+local a = 100000000
+
+require("bytedump")
+
+print("end")
