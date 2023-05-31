1bda250ed8f374397ee534487a52fa6220e702c5
v012345 <v138468@gmail.com>
2023-05-31 18:58:25 +0800

    加入 lfs

--- scripts/lua/parser_example.lua
+++ scripts/lua/parser_example.lua
@@ -1,3 +1,10 @@
-print("1")
-print(lfs)
-print("2")
+
+local file_path ="C:/Users/Meteor/Desktop/v2-1495b2ab24e5a03635e1b82cbd97b91e_r.jpg"
+
+-- 获取文件大小
+local file_size = lfs.attributes(file_path, "size")
+print("文件大小：" .. file_size .. " bytes")
+
+-- 获取文件修改日期
+local modification_time = lfs.attributes(file_path, "modification")
+print("文件修改日期：" .. os.date("%Y-%m-%d %H:%M:%S", modification_time))
