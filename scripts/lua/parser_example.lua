232765ddf2fd3a23e3ada54a9f477afa5434d086
v138468 <v138468@gmail.com>
2023-05-31 20:15:00 +0800

    comment

--- scripts/lua/parser_example.lua
+++ scripts/lua/parser_example.lua
@@ -1,10 +1,16 @@
 print("jiii")
 local file_path = "C:\\Users\\MH\\Desktop\\日语单词\\0117.xlsx"
 local lfs = require "lfs"
--- 获取文件大小
-local file_size = lfs.attributes(file_path, "size")
-print("文件大小：" .. file_size .. " bytes")
+print(lfs)
+local function main()
+    -- 获取文件大小
+    local file_size = lfs.attributes(file_path, "size")
+    print("文件大小：" .. file_size .. " bytes")
 
--- 获取文件修改日期
-local modification_time = lfs.attributes(file_path, "modification")
-print("文件修改日期：" .. os.date("%Y-%m-%d %H:%M:%S", modification_time))
+    -- 获取文件修改日期
+    local modification_time = lfs.attributes(file_path, "modification")
+    print("文件修改日期：" .. os.date("%Y-%m-%d %H:%M:%S", modification_time))
+end
+xpcall(main, function(a)
+    print(a)
+end)
