c6cf8050d31f2ad369288f1c7818ef90de9069f7
v012345 <v138468@gmail.com>
2023-05-31 15:21:42 +0800

    加入 lfs

--- modules/lfs.h
+++ modules/lfs.h
@@ -9,17 +9,17 @@
 
 /* Define 'chdir' for systems that do not implement it */
 #ifdef NO_CHDIR
-#define chdir(p)	(-1)
-#define chdir_error	"Function 'chdir' not provided by system"
+#define chdir(p) (-1)
+#define chdir_error "Function 'chdir' not provided by system"
 #else
-#define chdir_error	strerror(errno)
+#define chdir_error strerror(errno)
 #endif
 
 #ifdef _WIN32
 #define chdir(p) (_chdir(p))
 #define getcwd(d, s) (_getcwd(d, s))
 #define rmdir(p) (_rmdir(p))
-#define LFS_EXPORT __declspec (dllexport)
+#define LFS_EXPORT __declspec(dllexport)
 #ifndef fileno
 #define fileno(f) (_fileno(f))
 #endif
@@ -31,7 +31,7 @@
 extern "C" {
 #endif
 
-  LFS_EXPORT int luaopen_lfs(lua_State * L);
+LFS_EXPORT int luaopen_lfs(lua_State* L);
 
 #ifdef __cplusplus
 }
