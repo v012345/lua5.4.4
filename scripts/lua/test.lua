650321ecc6106c4bfa44b89c4b625f54d838db43
v012345 <v138468@gmail.com>
2023-06-12 18:39:34 +0800

    format

--- scripts/lua/test.lua
+++ scripts/lua/test.lua
@@ -1,36 +1,28 @@
-local function quick_sort(t, lo, up)
+local function quick_sort(t, lo, up, b)
     if lo < up then
-        local p = (lo + up) // 2
-
-        local vp = t[p]
-        local i = lo
+        local p = lo + 1
+        local vp = t[lo + 1]
+        local k = lo
         local j = up
-        while true do
-            while t[i] < vp do
-                i = i + 1
-            end
-            while t[j] >= vp do
-                j = j - 1
-            end
-            t[i], t[j] = t[j], t[i]
-            if i >= j then
-                p = i
-                break
-            end
+        for i = k, j do
+
         end
-        quick_sort(t, lo, p)
-        quick_sort(t, p, up)
+
+
+        quick_sort(t, lo, p, b)
+        quick_sort(t, p, up, b)
     end
 end
 
 local a = { 3, 4, 1, 5, 6, 2, 6, 9, 0, 6, 4, 6 }
+local b = {}
 -- local a = { 4, 1, 2 }
 debug.sethook(function(a, b)
     print(a, b)
 end, "l")
 xpcall(quick_sort, function(msg)
     print(msg)
-end, a, 1, #a)
+end, a, 1, #a, b)
 
 local t = {}
 for index, value in ipairs(a) do
