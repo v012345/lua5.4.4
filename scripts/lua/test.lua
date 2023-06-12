b96fa597b4c4ea8ab6e1e5af7f0b8bbb089313d9
v012345 <v138468@gmail.com>
2023-06-12 18:34:04 +0800

    format

--- scripts/lua/test.lua
+++ scripts/lua/test.lua
@@ -1,32 +1,36 @@
-debug.sethook(function(a, b)
-    print(a, b)
-end, "l")
+local function quick_sort(t, lo, up)
+    if lo < up then
+        local p = (lo + up) // 2
 
-local function partition(t, l, h, p)
-    while l < h do
-        while t[l] < p do
-            l = l + 1
+        local vp = t[p]
+        local i = lo
+        local j = up
+        while true do
+            while t[i] < vp do
+                i = i + 1
+            end
+            while t[j] >= vp do
+                j = j - 1
+            end
+            t[i], t[j] = t[j], t[i]
+            if i >= j then
+                p = i
+                break
+            end
         end
-        while t[h] > p do
-            h = h - 1
-        end
-        t[l], t[h] = t[h], t[l]
-    end
-    return l
-end
-
-local function quick_sort(t, l, h)
-    if l < h then
-        local i = math.random(l, h)
-        local j = partition(t, l, h, i)
-        quick_sort(t, l, j)
-        quick_sort(t, j, h)
+        quick_sort(t, lo, p)
+        quick_sort(t, p, up)
     end
 end
 
 local a = { 3, 4, 1, 5, 6, 2, 6, 9, 0, 6, 4, 6 }
-
-quick_sort(a, 1, #a)
+-- local a = { 4, 1, 2 }
+debug.sethook(function(a, b)
+    print(a, b)
+end, "l")
+xpcall(quick_sort, function(msg)
+    print(msg)
+end, a, 1, #a)
 
 local t = {}
 for index, value in ipairs(a) do
