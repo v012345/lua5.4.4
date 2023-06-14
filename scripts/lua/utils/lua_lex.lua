3cd64f1b6c5fc8a8d841f28b08e5b08d8e6d3c0b
v012345 <v138468@gmail.com>
2023-06-14 16:24:51 +0800

    format

--- scripts/lua/utils/lua_lex.lua
+++ scripts/lua/utils/lua_lex.lua
@@ -1,203 +1,191 @@
-local Lex = {
-    file = nil,
-    linenumber = 0,
-    position = 1,
-    linechars = 0,
-    current = "",
+---@class LexState
+local LexState = {
+    line_number = 0,
+    char_pointer = 1,
+    char_position = 1,
+    stream_len = 0,
+    ---@type string|number
+    current_char = "",
     stream = nil,
     type = {
-        reserved = 0,
-        string = 1,
-        number = 2,
-        other = 5,
+        reserved = 1,
+        string = 2,
+        number = 3,
+        symbol = 4,
+        name = 5,
+        other = 6,
+        end_of_file = -1,
     },
-    END_OF_FILE = "\n==END_OF_FILE==\n",
-    t = {}
+    token = {
+        type = 1,
+        value = 1,
+    },
+    ahead_token = {
+        type = 1,
+        value = 1,
+    },
 }
-local END_OF_FILE = "\n==END_OF_FILE==\n"
 
-function Lex:load(file_path)
+function LexState:load(file_path)
     local file = io.open(file_path, "r")
     if file then
-        self.file = file
-        Lex:get_a_char()
+        self.stream = file:read("a")
+        file:close()
+        self.stream_len = #self.stream
+        self.ahead_token = nil
+        self.token = nil
+        self.current_char = nil
+        LexState:get_a_char()
     else
-        error("Lex can't open " .. file_path)
+        error("LexState can't open " .. file_path)
     end
 end
 
-function Lex:look_next_char()
-    return string.sub(self.stream, self.position + 1, self.position + 1)
-end
-
-function Lex:get_a_char()
-    if self.linechars <= 0 then
-        self.stream = self.file:read("L")
-        if not self.stream then
-            self.current = END_OF_FILE
-            return END_OF_FILE
-        end
-        self.linechars = #self.stream
-        self.position = 1
+function LexState:get_a_char()
+    if self.char_pointer >= self.stream_len then
+        self.current_char = self.type.end_of_file
+        return self.type.end_of_file
     end
-    local p = self.position
+    local p = self.char_pointer
     local c = string.sub(self.stream, p, p)
-    self.position = p + 1
-    self.linechars = self.linechars - 1
-    self.current = c
+    self.char_pointer = p + 1
+    self.current_char = c
     return c
 end
 
-function Lex:skip_the_line()
-    self.linechars = 0
+function LexState:skip_the_line()
+    while self.current_char ~= "\n" and self.current_char ~= self.type.end_of_file do
+        self:get_a_char()
+    end
 end
 
-function Lex:test()
-    -- local c = self:next()
-    -- while c.token ~= END_OF_FILE do
-    --     print(self.linenumber)
-    --     c = self:next()
-    -- end
+function LexState:set_token(type, value)
+    self.token = {
+        type = type,
+        value = value,
+    }
+    return self.token
 end
 
-function Lex:next()
-    local c = self.current
-    while c ~= END_OF_FILE do
+function LexState:test()
+    local c = self:get_next_token()
+    local l = self.line_number
+    while c.type ~= self.type.end_of_file do
+        if self.line_number > l then
+            l = self.line_number
+            io.write("\n")
+        end
+        io.write(" ")
+        io.write(c.value, c.type)
+        c = self:get_next_token()
+    end
+end
+
+function LexState:get_next_token()
+    local c = self.current_char
+    while self.current_char ~= self.type.end_of_file do
+        c = self.current_char
         if c == "\n" or c == "\r" then
-            self.linenumber = self.linenumber + 1
+            self.line_number = self.line_number + 1
             c = self:get_a_char()
         elseif c == " " or c == "\f" or c == "\t" or c == "\v" then
             c = self:get_a_char()
         elseif c == "-" then
             c = self:get_a_char()
             if c ~= "-" then
-                return self:token("-")
+                return self:set_token(self.type.other, "-")
             end
             self:skip_the_line() -- 只支持单行注释
-            c = self:get_a_char()
         elseif c == "[" then
             c = self:get_a_char()
-            return self:token("[")
+            return self:set_token(self.type.other, "[")
         elseif c == "=" then
             c = self:get_a_char()
             if c == "=" then
                 c = self:get_a_char()
-                return self:token("==")
+                return self:set_token(self.type.other, "==")
             else
-                return self:token("=")
+                return self:set_token(self.type.other, "=")
             end
         elseif c == "<" then
             c = self:get_a_char()
             if c == "=" then
                 c = self:get_a_char()
-                return self:token("<=")
+                return self:set_token(self.type.other, "<=")
             elseif c == "<" then
                 c = self:get_a_char()
-                return self:token("<<")
+                return self:set_token(self.type.other, "<<")
             else
-                return self:token("<")
+                return self:set_token(self.type.other, "<")
             end
         elseif c == ">" then
             c = self:get_a_char()
             if c == "=" then
                 c = self:get_a_char()
-                return self:token(">=")
+                return self:set_token(self.type.other, ">=")
             elseif c == ">" then
                 c = self:get_a_char()
-                return self:token(">>")
+                return self:set_token(self.type.other, ">>")
             else
-                return self:token(">")
+                return self:set_token(self.type.other, ">")
             end
         elseif c == "/" then
             c = self:get_a_char()
             if c == "/" then
                 c = self:get_a_char()
-                return self:token("//")
+                return self:set_token(self.type.other, "//")
             else
-                return self:token("/")
+                return self:set_token(self.type.other, "/")
             end
         elseif c == "~" then
             c = self:get_a_char()
             if c == "=" then
                 c = self:get_a_char()
-                return self:token("~=")
+                return self:set_token(self.type.other, "~=")
             else
-                return self:token("~")
+                return self:set_token(self.type.other, "~")
             end
         elseif c == ":" then
             c = self:get_a_char()
             if c == ":" then
                 c = self:get_a_char()
-                return self:token("::")
+                return self:set_token(self.type.other, "::")
             else
-                return self:token(":")
+                return self:set_token(self.type.other, ":")
             end
         elseif c == "\"" or c == "'" then
-            self.t = {
-                token = self.type.string,
-                value = self:read_string(c),
-                line = self.linenumber
-            }
-            return self.t
+            return self:set_token(self.type.string, self:read_string(c))
         elseif c == "." then
             c = self:get_a_char()
             if c == "." then
                 c = self:get_a_char()
                 if c == "." then
                     c = self:get_a_char()
-                    return self:token("...")
+                    return self:set_token(self.type.other, "...")
                 else
-                    return self:token("..")
+                    return self:set_token(self.type.other, "..")
                 end
             else
-                return self:token(".") -- 不支持 .122 这么定义小数
+                return self:set_token(self.type.other, ".") -- 不支持 .122 这么定义小数
             end
         elseif c == "0" or c == "1" or c == "2" or c == "3" or c == "4" or c == "5" or c == "6" or c == "7" or c == "8" or c == "9" then
-            self.t = {
-                token = self.type.number,
-                value = self:read_number(),
-                line = self.linenumber
-            }
-            return self.t
+            return self:set_token(self.type.number, self:read_number())
         else
             local b = string.byte(c)
             if b >= 65 and b <= 90 or b >= 97 and b <= 122 then
                 local type, value = self:read_name()
-                if type then
-                    self.t = {
-                        token = self.type.reserved,
-                        value = value,
-                        line = self.linenumber
-                    }
-                    return self.t
-                else
-                    self.t = {
-                        token = self.type.other,
-                        value = value,
-                        line = self.linenumber
-                    }
-                    return self.t
-                end
+                return self:set_token(type, value)
             else
                 local t = c
                 c = self:get_a_char()
-                return self:token(t)
+                return self:set_token(self.type.other, t)
             end
         end
     end
-    return self:token(c)
+    return self:set_token(self.type.end_of_file, self.type.end_of_file)
 end
 
-function Lex:token(c)
-    self.t = {
-        token = c,
-        value = c,
-        line = self.linenumber
-    }
-    return self.t
-end
-
-function Lex:read_number() -- 只支持 10 进制小数
+function LexState:read_number() -- 只支持 10 进制小数
     local m = {
         ["0"] = "0",
         ["1"] = "0",
@@ -211,7 +199,7 @@ function Lex:read_number() -- 只支持 10 进制小数
         ["9"] = "0",
         ["."] = "0",
     }
-    local c = self.current
+    local c = self.current_char
     local t = {}
     while m[c] do
         t[#t + 1] = c
@@ -220,7 +208,7 @@ function Lex:read_number() -- 只支持 10 进制小数
     return tonumber(table.concat(t))
 end
 
-function Lex:read_string(d) -- 不允许转义字符出现, 不想写
+function LexState:read_string(d) -- 不允许转义字符出现, 不想写
     local c = self:get_a_char()
     local t = {}
     while c ~= d do
@@ -231,34 +219,35 @@ function Lex:read_string(d) -- 不允许转义字符出现, 不想写
     return table.concat(t)
 end
 
-function Lex:read_name(d) -- 用空格分隔
-    local c = self.current
+function LexState:read_name(d) -- 用空格分隔
+    local c = self.current_char
     local t = {}
+    local r = self.type.reserved
     local m = {
-        ["and"] = 0,
-        ["break"] = 0,
-        ["do"] = 0,
-        ["else"] = 0,
-        ["elseif"] = 0,
-        ["end"] = 0,
-        ["false"] = 0,
-        ["for"] = 0,
-        ["function"] = 0,
-        ["goto"] = 0,
-        ["if"] = 0,
-        ["in"] = 0,
-        ["local"] = 0,
-        ["nil"] = 0,
-        ["not"] = 0,
-        ["or"] = 0,
-        ["repeat"] = 0,
-        ["return"] = 0,
-        ["then"] = 0,
-        ["true"] = 0,
-        ["until"] = 0,
-        ["while"] = 0,
+        ["and"] = r,
+        ["break"] = r,
+        ["do"] = r,
+        ["else"] = r,
+        ["elseif"] = r,
+        ["end"] = r,
+        ["false"] = r,
+        ["for"] = r,
+        ["function"] = r,
+        ["goto"] = r,
+        ["if"] = r,
+        ["in"] = r,
+        ["local"] = r,
+        ["nil"] = r,
+        ["not"] = r,
+        ["or"] = r,
+        ["repeat"] = r,
+        ["return"] = r,
+        ["then"] = r,
+        ["true"] = r,
+        ["until"] = r,
+        ["while"] = r,
     }
-    while c ~= " " and c ~= END_OF_FILE do
+    while c ~= " " and c ~= self.type.end_of_file do
         if string.match(c, "[0-9a-zA-Z_]") then
             t[#t + 1] = c
             c = self:get_a_char()
@@ -267,16 +256,30 @@ function Lex:read_name(d) -- 用空格分隔
         end
     end
     local name = table.concat(t)
-
-    return m[name], name
+    if m[name] then
+        return m[name], name
+    else
+        return self.type.name, name
+    end
 end
 
-function Lex:run()
-    -- local l = self.linenumber
+function LexState:error(what)
+    error(string.format("line:%s, %s", self.line_number, what))
 end
 
--- function Lex:lookahead()
+function LexState:test_next_token(type, value)
+    local char_pointer = self.char_pointer
+    local current_char = self.current_char
+    local line_number = self.line_number
+    local token = self:get_next_token()
+    self.char_pointer = char_pointer
+    self.current_char = current_char
+    self.line_number = line_number
 
--- end
+    if token.type == type and token.value == value then
+        return true
+    end
+    return false
+end
 
-return Lex
+return LexState
