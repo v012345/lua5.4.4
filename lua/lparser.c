254e9d0db199d1fbe4cb45eb876991b8bdbc97f1
v012345 <v138468@gmail.com>
2023-06-16 16:03:28 +0800

    format

--- lua/lparser.c
+++ lua/lparser.c
@@ -1311,7 +1311,8 @@ static void checkrepeated(LexState* ls, TString* name) {
 static void labelstat(LexState* ls, TString* name, int line) {
     /* label -> '::' NAME '::' */
     checknext(ls, TK_DBCOLON); /* skip double colon */
-    while (ls->t.token == ';' || ls->t.token == TK_DBCOLON) statement(ls); /* skip other no-op statements */
+    while (ls->t.token == ';' || ls->t.token == TK_DBCOLON) // 跳结束的 ::
+        statement(ls); /* skip other no-op statements */
     checkrepeated(ls, name); /* check for repeated labels */
     createlabel(ls, name, line, block_follow(ls, 0));
 }
--- scripts/lua/test.lua
+++ scripts/lua/test.lua
@@ -1,18 +1,25 @@
-local lLex = require("utils.lua_lex")
-lLex:load("utils/json_lex.lua")
--- debug.sethook(function(a, b)
---     print(a, b)
--- end, "l", 0)
--- xpcall(lLex.test, function(a)
+local t = { 1, 2, 3 }
+for key, value, a, b, c in pairs(t) do
+    print(key, value, a, b, c)
+end
+function t.jiii()
+    
+end 
+-- local lLex = require("utils.lua_lex")
+-- lLex:load("utils/json_lex.lua")
+-- -- debug.sethook(function(a, b)
+-- --     print(a, b)
+-- -- end, "l", 0)
+-- -- xpcall(lLex.test, function(a)
+-- --     print("\n")
+-- --     print(a)
+-- -- end, lLex)
+-- -- do
+-- --     return
+-- -- end
+-- local lParser = require("utils.lua_parser")
+-- lParser:init(lLex)
+-- xpcall(lParser.test, function(a)
 --     print("\n")
 --     print(a)
--- end, lLex)
--- do
---     return
--- end
-local lParser = require("utils.lua_parser")
-lParser:init(lLex)
-xpcall(lParser.test, function(a)
-    print("\n")
-    print(a)
-end, lParser)
+-- end, lParser)
