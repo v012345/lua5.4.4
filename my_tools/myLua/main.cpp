0814b1d4bd1e187ac4b9d836840e5c538ca00632
v012345 <v138468@gmail.com>
2023-06-20 10:47:42 +0800

    format

--- my_tools/myLua/main.cpp
+++ my_tools/myLua/main.cpp
@@ -1,15 +1,17 @@
-#include <filesystem>
 #include <lua.hpp>
 extern "C" {
 #include <lfs.h>
 }
-#define UNICODE
-#include <windows.h>
 #define LUA_MAIN_SCRIPT "./test.lua"
+void print_code(lua_State* L);
 int main(int argc, char const* argv[]) {
     L = luaL_newstate();
     luaL_openlibs(L);
     luaopen_lfs(L);
-    luaL_dofile(L, LUA_MAIN_SCRIPT); //
+    luaL_loadfile(L, LUA_MAIN_SCRIPT);
+    lua_pcall(L, 0, LUA_MULTRET, 0);
     return 0;
+}
+void print_code(lua_State* L) {
+    
 }
