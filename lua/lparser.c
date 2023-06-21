c95ce6f356340b193e12d83648a64c87c88790d2
v012345 <v138468@gmail.com>
2023-06-21 14:58:28 +0800

    format

--- lua/lparser.c
+++ lua/lparser.c
@@ -111,7 +111,9 @@ static void check_match(LexState* ls, int what, int who, int where) { // 😊
     if (l_unlikely(!testnext(ls, what))) {
         if (where == ls->linenumber) /* all in the same line? */
             error_expected(ls, what); /* do not need a complex message */
-        else { luaX_syntaxerror(ls, luaO_pushfstring(ls->L, "%s expected (to close %s at line %d)", luaX_token2str(ls, what), luaX_token2str(ls, who), where)); }
+        else { //
+            luaX_syntaxerror(ls, luaO_pushfstring(ls->L, "%s expected (to close %s at line %d)", luaX_token2str(ls, what), luaX_token2str(ls, who), where));
+        }
     }
 }
 
