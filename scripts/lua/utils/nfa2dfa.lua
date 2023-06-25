b0a09a05ec2d773a55ab797f1c0a88955439373d
v012345 <v138468@gmail.com>
2023-06-25 15:51:15 +0800

    dot

--- scripts/lua/utils/nfa2dfa.lua
+++ scripts/lua/utils/nfa2dfa.lua
@@ -16,18 +16,30 @@ local function get_a_state()
     return tostring(x)
 end
 
+local function basic_convert(NFA)
+    local __matrix = NFA.__matrix
+    local matrix = {}
+    for from, row in pairs(__matrix) do
+        for lable, to in pairs(row) do
+            if lable == "" then -- ε
+            elseif string.match(lable, '|') then
+                error("nfa2dfa|")
+            elseif string.match(lable, '*') then
+                error("nfa2dfa*")
+            elseif #lable > 1 then
+            else
+                matrix[from] = matrix[from] or {}
+                matrix[from][lable] = to
+            end
+        end
+    end
+    NFA.__matrix = matrix
+end
+
 local function nfa2dfa(NFA)
-    local DFA = {}
-    DFA.__name = NFA.__name
-    DFA.__size = NFA.__size
-    DFA.__rankdir = NFA.__rankdir
     set_temp_states(NFA)
-    local start_state         = get_a_state()
-    local end_state           = get_a_state()
-    DFA.__start               = {}
-    DFA.__end                 = {}
-    DFA.__start[start_state]  = true
-    DFA.__end[end_state]      = true
+    local start_state = get_a_state()
+    local end_state = get_a_state()
     NFA.__matrix[start_state] = {}
     for state, value in pairs(NFA.__start) do
         NFA.__matrix[start_state][""] = state
@@ -36,23 +48,11 @@ local function nfa2dfa(NFA)
     for state, value in pairs(NFA.__end) do
         NFA.__matrix[state][""] = end_state
     end
-
     NFA.__start = {}
     NFA.__end = {}
     NFA.__start[start_state] = true
     NFA.__end[end_state] = true
-    -- for from, row in pairs(NFA.__matrix) do
-    --     for lable, to in pairs(row) do
-    --         if lable == "" then -- ε
-    --         elseif string.match(lable, '|') then
-    --             error("nfa2dfa|")
-    --         elseif string.match(lable, '*') then
-    --             error("nfa2dfa*")
-    --         elseif #lable > 1 then
-    --         end
-    --     end
-    -- end
-    return DFA
+    basic_convert(NFA)
 end
 
 return nfa2dfa
