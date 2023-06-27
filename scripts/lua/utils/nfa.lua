3296165210ca7a5104ffbf1e16fd64b072646289
v012345 <v138468@gmail.com>
2023-06-27 16:45:44 +0800

    1

--- scripts/lua/utils/nfa.lua
+++ scripts/lua/utils/nfa.lua
@@ -66,6 +66,55 @@ function mt.remove_final_states(NFA, states)
     NFA.states:remove(states)
 end
 
+---comment
+---@param NFA NFA
+function mt.__tostring(NFA)
+    local escape_r = {
+        ["\\"] = "\\\\",
+        ["\""] = "\\\"",
+        ["/"] = "\\/",
+        ["\r"] = "\\r",
+        ["\f"] = "\\f",
+        ["\n"] = "\\n",
+        ["\t"] = "\\t",
+        ["\b"] = "\\b",
+    }
+
+    local function escape_string(str)
+        local o = {}
+        for i = 1, #str do
+            local char = string.sub(str, i, i)
+            if escape_r[char] then
+                o[i] = escape_r[char]
+            else
+                o[i] = char
+            end
+        end
+        return table.concat(o)
+    end
+    local t = {}
+    t[#t + 1] = "digraph nfa {\n"
+    t[#t + 1] = "    rankdir = LR;\n"
+    t[#t + 1] = "    size = \"8,5\";\n"
+    t[#t + 1] = "    node [shape = doublecircle;];\n"
+    for k in pairs(NFA.initial_states) do
+        t[#t + 1] = string.format("    %s [color = green;];\n", k)
+    end
+    for k in pairs(NFA.final_states) do
+        t[#t + 1] = string.format("    %s [color = red;];\n", k)
+    end
+    t[#t + 1] = string.format("    node [shape = circle;];\n")
+    for from_state, row in pairs(NFA.transition_matrix) do
+        for lable, to_states in pairs(row) do
+            for to_state in pairs(to_states) do
+                t[#t + 1] = string.format("    %s -> %s [label = \"%s\";];\n", from_state, to_state, escape_string(lable))
+            end
+        end
+    end
+    t[#t + 1] = "}\n"
+    return table.concat(t)
+end
+
 return function()
     ---@class NFA
     local NFA = {
@@ -76,7 +125,8 @@ return function()
         final_states = set(),
     }
     setmetatable(NFA, {
-        __index = mt
+        __index = mt,
+        __tostring = mt.__tostring,
     })
     return NFA
 end
