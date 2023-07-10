fb7bbf056df9d6f390202ba99001edc03561704d
v012345 <v138468@gmail.com>
2023-07-10 10:51:47 +0800

    1

--- lua/lgc.c
+++ lua/lgc.c
@@ -1532,6 +1532,7 @@ void luaC_runtilstate(lua_State* L, int statesmask) {
 ** controls when next step will be performed.
 */
 static void incstep(lua_State* L, global_State* g) {
+    // 倍率
     int stepmul = (getgcparam(g->gcstepmul) | 1); /* avoid division by 0 */
     l_mem debt = (g->GCdebt / WORK2MEM) * stepmul;
     l_mem stepsize = (g->gcstepsize <= log2maxs(l_mem)) ? ((cast(l_mem, 1) << g->gcstepsize) / WORK2MEM) * stepmul : MAX_LMEM; /* overflow; keep maximum value */
