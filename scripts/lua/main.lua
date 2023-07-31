ddcb6655571c0a855f0e49a7d0336d4e74092777
v012345 <v138468@gmail.com>
2023-07-31 11:56:50 +0800

    1

--- scripts/lua/main.lua
+++ scripts/lua/main.lua
@@ -1,57 +1,115 @@
-local function div_code(file, codes)
-    file:write('<div class="code-container">')
-    for i, code in ipairs(codes) do
-        file:write('<div class="code">')
-        file:write(code)
-        file:write('</div>')
-    end
-    file:write('</div>')
+local function html_css(file)
+
 end
 
-local function div_k(file, const)
-    file:write('<div class="const-container">')
-    for i, k in ipairs(const) do
-        file:write('<div class="const">')
-        file:write(k)
-        file:write('</div>')
-    end
-    file:write('</div>')
+local function html_head(file)
+    file:write([[
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>lua</title>
+        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+        <style>
+    ]])
+    html_css(file)
+    file:write([[
+        </style>
+        </head>
+    ]])
 end
 
-local function div_upvalue(file, upvalues)
-    file:write('<div class="upvalue-container">')
-    for i, upvalue in ipairs(upvalues) do
-        file:write('<div class="upvalue">')
-        file:write(tostring(upvalue))
-        file:write('</div>')
-    end
-    file:write('</div>')
-end
-
-xpcall(function()
-    local p1 = Compile("./clua.lua")
-    require "bytedump"
-    local o = io.open("./dump.html", "w") or error()
-    o:write("top level function: \n")
-    local function printP(p, output, tab)
-        local vars = p.locvars
-        output:write(string.rep("\t", tab))
-        output:write("vars :\t")
-        for _, value in ipairs(vars) do
-            output:write(value)
-            output:write("\t")
+local function html_body(file)
+    local function div_code(file, codes)
+        file:write('<div class="code-container container">')
+        for i, code in ipairs(codes) do
+            file:write('<div class="code">')
+            file:write(code)
+            file:write('</div>')
         end
-        output:write("\n")
+        file:write('</div>')
+    end
 
+    local function div_k(file, const)
+        file:write('<div class="const-container container">')
+        for i, k in ipairs(const) do
+            file:write('<div class="const">')
+            file:write(k)
+            file:write('</div>')
+        end
+        file:write('</div>')
+    end
+
+    local function div_upvalue(file, upvalues)
+        file:write('<div class="upvalue-container container">')
+        for i, upvalue in ipairs(upvalues) do
+            file:write('<div class="upvalue">')
+            file:write(tostring(upvalue))
+            file:write('</div>')
+        end
+        file:write('</div>')
+    end
+
+    local function div_locvar(file, locvars)
+        file:write('<div class="locvar-container container">')
+        for i, locvar in ipairs(locvars) do
+            file:write('<div class="locvar">')
+            file:write(tostring(locvar))
+            file:write('</div>')
+        end
+        file:write('</div>')
+    end
+
+    local p1 = Compile("./clua.lua")
+    local function div_closure(p, output)
+        file:write('<div class="closure-container container">')
+        div_locvar(output, p.locvars)
         div_k(output, p.k)
         div_upvalue(output, p.upvalues)
         div_code(output, p.code)
-        output:write("p :\n")
         for key, value in pairs(p.p) do
-            printP(value, output, tab + 1)
+            div_closure(value, output)
         end
+        file:write('</div>')
     end
-    printP(p1, o, 0)
+    file:write("<body>")
+    file:write('<div id="app">')
+    file:write('{{ message }}')
+    div_closure(p1, file)
+    file:write('</div>')
+    file:write("</body>")
+end
+
+local function html_script(file)
+    file:write("<script>")
+    file:write([[
+        const { createApp, ref } = Vue
+
+        createApp({
+          setup() {
+            const message = ref('Top level function!')
+            return {
+              message
+            }
+          }
+        }).mount('#app')
+    ]])
+    file:write("</script>")
+end
+
+local function html(file)
+    file:write([[
+        <!DOCTYPE html>
+        <html lang="en">
+    ]])
+    html_head(file)
+    html_body(file)
+    html_script(file)
+    file:write("</html>")
+end
+
+xpcall(function()
+    local o = io.open("./dump.html", "w") or error()
+    html(o)
     o:close()
 end, function(msg)
     print(msg)
