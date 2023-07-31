330a1858f5d5bffae4d8bd8b4e1fa49f6d2f63fd
v012345 <v138468@gmail.com>
2023-07-31 15:52:24 +0800

    1

--- scripts/lua/main.lua
+++ scripts/lua/main.lua
@@ -1,117 +1,36 @@
-local function html_css(file)
-
-end
-
-local function html_head(file)
-    file:write([[
-        <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>lua</title>
-        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-        <style>
-    ]])
-    html_css(file)
-    file:write([[
-        </style>
-        </head>
-    ]])
-end
-
-local function html_body(file)
-    local function div_code(file, codes)
-        file:write('<div class="code-container container">')
-        for i, code in ipairs(codes) do
-            file:write('<div class="code">')
-            file:write(code)
-            file:write('</div>')
+local function html(file, h5)
+    file:write("<!DOCTYPE html>")
+    local function tofile(file, tag)
+        print(tag.name)
+        if tag.name then
+            file:write("<")
+            file:write(tag.name)
+            file:write(" ")
+            for key, value in pairs(tag.attributes) do
+                if value then
+                    file:write(string.format("%s=\"%s\" ", key, value))
+                else
+                    file:write(key)
+                    file:write(" ")
+                end
+            end
+            file:write(">")
+            for _, sub_tag in ipairs(tag.children) do
+                tofile(file, sub_tag)
+            end
+            file:write(string.format("</%s>", tag.name))
+        else
+            file:write(tag.children[1])
         end
-        file:write('</div>')
     end
-
-    local function div_k(file, const)
-        file:write('<div class="const-container container">')
-        for i, k in ipairs(const) do
-            file:write('<div class="const">')
-            file:write(k)
-            file:write('</div>')
-        end
-        file:write('</div>')
-    end
-
-    local function div_upvalue(file, upvalues)
-        file:write('<div class="upvalue-container container">')
-        for i, upvalue in ipairs(upvalues) do
-            file:write('<div class="upvalue">')
-            file:write(tostring(upvalue))
-            file:write('</div>')
-        end
-        file:write('</div>')
-    end
-
-    local function div_locvar(file, locvars)
-        file:write('<div class="locvar-container container">')
-        for i, locvar in ipairs(locvars) do
-            file:write('<div class="locvar">')
-            file:write(tostring(locvar))
-            file:write('</div>')
-        end
-        file:write('</div>')
-    end
-
-    local p1 = Compile("./clua.lua")
-    local function div_closure(p, output)
-        file:write('<div class="closure-container container">')
-        div_locvar(output, p.locvars)
-        div_k(output, p.k)
-        div_upvalue(output, p.upvalues)
-        div_code(output, p.code)
-        for key, value in pairs(p.p) do
-            div_closure(value, output)
-        end
-        file:write('</div>')
-    end
-    file:write("<body>")
-    file:write('<div id="app">')
-    file:write('{{ message }}')
-    div_closure(p1, file)
-    file:write('</div>')
-    file:write("</body>")
-end
-
-local function html_script(file)
-    file:write("<script>")
-    file:write([[
-        const { createApp, ref } = Vue
-
-        createApp({
-          setup() {
-            const message = ref('Top level function!')
-            return {
-              message
-            }
-          }
-        }).mount('#app')
-    ]])
-    file:write("</script>")
-end
-
-local function html(file)
-    file:write([[
-        <!DOCTYPE html>
-        <html lang="en">
-    ]])
-    html_head(file)
-    html_body(file)
-    html_script(file)
-    file:write("</html>")
+    tofile(file, h5[1])
 end
 
 xpcall(function()
     local h5 = require "utils.html2table"
-    h5("./h5.html")
+    local lh5 = h5("./h5.html")
     local o = io.open("./dump.html", "w") or error()
-    html(o)
+    html(o, lh5)
     o:close()
 end, function(msg)
     print(msg)
