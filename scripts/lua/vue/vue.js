42ba4f1bdc752255691458ed2de4c6edc41a673f
v012345 <v138468@gmail.com>
2023-08-04 12:05:09 +0800

    1

--- scripts/lua/vue/vue.js
+++ scripts/lua/vue/vue.js
@@ -2,13 +2,16 @@ const { createApp, ref } = Vue
 axios.get('./lua.json')
     .then(res => console.log(res.data))
     .catch(err => console.log(err))
-createApp({
+console.log(ElementPlus)
+const app = createApp({
     setup() {
         const message = ref('Top level function!')
         return {
             message
         }
     }
-}).mount('#app')
+})
+app.use(ElementPlus)
+app.mount('#app')
 
 
