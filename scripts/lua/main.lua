515e1285d4a21417c9e6272ee5a35087983a150c
v012345 <v138468@gmail.com>
2023-08-21 10:11:55 +0800

    1

--- scripts/lua/main.lua
+++ scripts/lua/main.lua
@@ -27,6 +27,7 @@ local function main()
     local base = "zhcn"
     local langs = { "en", "id", "th", "vi" }
     if arg["extract"] then
+        print(">>> start extracting >>>")
         local csv = io.open("./trans.csv", "w") or error("can't open trans.csv")
         csv:write(string.format('"%s"', base), ",")
         for _, lang in ipairs(langs) do
@@ -36,13 +37,14 @@ local function main()
 
         local function extract(csd_name, base_node, ...)
             local langs_node = table.pack(...)
-            local ex_format = ',"%s","%s","%s","%s"\n'
+            local ex_format = '"%s","%s","%s","%s"\n'
             local function tocsv(attribute)
                 csv:write('"', base_node.attributes[attribute], '"', ",")
                 for _, lang_node in ipairs(langs_node) do
                     csv:write('"', lang_node.attributes[attribute], '"', ",")
                 end
-                csv:write(string.format(ex_format, csd_name, base_node.name, attribute, base_node.attributes["Tag"]))
+                csv:write(string.format(ex_format, csd_name, base_node.attributes["Name"], attribute,
+                    base_node.attributes["Tag"]))
             end
 
             if base_node.attributes["ButtonText"] then
@@ -69,7 +71,10 @@ local function main()
         for i, lang in ipairs(langs) do
             langs_ui[i] = getFiles(lang)
         end
+        local index = 1
         for csd_name, csd_path in pairs(base_ui) do
+            print(index, csd_name)
+            index = index + 1
             local base_node = xml(csd_path)
             local langs_node = {}
             for i = 1, #langs_ui, 1 do
@@ -77,6 +82,7 @@ local function main()
             end
             extract(csd_name, base_node, table.unpack(langs_node))
         end
+        print("<<< end extracting <<<")
         csv:close()
     elseif arg["replace"] then
         local csv = io.open("./trans.csv", "r") or error("can't open trans.csv")
