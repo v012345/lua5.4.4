77ed06e05143f4b2b57caa2d3b26b44c40c2b0ba
v012345 <v138468@gmail.com>
2023-10-08 14:59:10 +0800

    utf16 到 unicode

--- test/lib.lua
+++ test/lib.lua
@@ -1,6 +1,17 @@
 Global = {}
 function Global.wchar_to_utf8(ws)
+    local unicode = 0
     for index, value in ipairs(ws) do
-        print(index, string.format("%x", value))
+        if 0xDC00 <= value and value <= 0xDFFF then
+            -- Skip Low Surrogate
+        else
+            unicode = value
+            if 0xD800 <= value and value <= 0xDBFF then
+                local high = value - 0xD800
+                local low = ws[index + 1] - 0xDC00
+                unicode = (high << 10) + low
+            end
+            print(index, unicode)
+        end
     end
 end
